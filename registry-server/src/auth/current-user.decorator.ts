4aa0f5141f7a3a57220df99f6d8637e746d9f7fe
KobepayTech <sosteristephen@gmail.com>
2026-05-17 15:31:39 +0000

    revert: remove registry-server (using full kobeOs server on Render instead)
    Co-authored-by: Ona <no-reply@ona.com>

--- registry-server/src/auth/current-user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { RegistryUser } from './user.entity';
-
-export const CurrentUser = createParamDecorator(
-  (field: keyof RegistryUser | undefined, ctx: ExecutionContext) => {
-    const user = ctx.switchToHttp().getRequest().user as RegistryUser;
-    return field ? user?.[field] : user;
-  },
-);
