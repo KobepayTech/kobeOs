00e4be637015acda6829facbf9a8b9e53404aa46
KobepayTech <159016310+KobepayTech@users.noreply.github.com>
2026-05-28 19:46:45 +0300

    Register RuView signal ingestion service

--- server/src/kobe-security/kobe-security.module.ts
+++ server/src/kobe-security/kobe-security.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SecurityClient } from './security-client.entity';
 import { ClientSite } from './client-site.entity';
@@ -8,6 +9,7 @@ import { ServiceCheck } from './service-check.entity';
 import { SiteSignal } from './site-signal.entity';
 import { WorkItem } from './work-item.entity';
 import { KobeSecurityController } from './kobe-security.controller';
+import { RuViewSignalIngestService } from './signal-ingest.service';
 import {
   ClientSitesService,
   KobeSecurityDashboardService,
@@ -21,6 +23,7 @@ import {
 
 @Module({
   imports: [
+    ConfigModule,
     TypeOrmModule.forFeature([
       SecurityClient,
       ClientSite,
@@ -41,6 +44,7 @@ import {
     SiteSignalsService,
     WorkItemsService,
     KobeSecurityDashboardService,
+    RuViewSignalIngestService,
   ],
 })
 export class KobeSecurityModule {}
