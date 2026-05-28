1fcfc6d43dd2a9968b54af9d8853eb511716775e
KobepayTech <159016310+KobepayTech@users.noreply.github.com>
2026-05-28 19:48:16 +0300

    Register Studio Media job runner

--- server/src/studio-media/studio-media.module.ts
+++ server/src/studio-media/studio-media.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudioMediaProject } from './media-project.entity';
 import { StudioMediaJob } from './media-job.entity';
 import { StudioMediaController } from './studio-media.controller';
 import { StudioMediaDashboardService, StudioMediaJobsService, StudioMediaProjectsService } from './studio-media.service';
+import { StudioMediaRunnerService } from './studio-media-runner.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([StudioMediaProject, StudioMediaJob])],
+  imports: [ConfigModule, TypeOrmModule.forFeature([StudioMediaProject, StudioMediaJob])],
   controllers: [StudioMediaController],
-  providers: [StudioMediaProjectsService, StudioMediaJobsService, StudioMediaDashboardService],
+  providers: [
+    StudioMediaProjectsService,
+    StudioMediaJobsService,
+    StudioMediaDashboardService,
+    StudioMediaRunnerService,
+  ],
 })
 export class StudioMediaModule {}
