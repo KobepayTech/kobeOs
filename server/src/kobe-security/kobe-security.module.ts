bf34693ea0b1f5715fe99e854d5a9759b99fea0b
KobepayTech <159016310+KobepayTech@users.noreply.github.com>
2026-05-28 17:43:38 +0300

    Add Kobe Security backend module

---
+++ server/src/kobe-security/kobe-security.module.ts
@@ -0,0 +1,46 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SecurityClient } from './security-client.entity';
+import { ClientSite } from './client-site.entity';
+import { TeamMember } from './team-member.entity';
+import { ServiceRoute } from './service-route.entity';
+import { ServiceCheck } from './service-check.entity';
+import { SiteSignal } from './site-signal.entity';
+import { WorkItem } from './work-item.entity';
+import { KobeSecurityController } from './kobe-security.controller';
+import {
+  ClientSitesService,
+  KobeSecurityDashboardService,
+  SecurityClientsService,
+  ServiceChecksService,
+  ServiceRoutesService,
+  SiteSignalsService,
+  TeamMembersService,
+  WorkItemsService,
+} from './kobe-security.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      SecurityClient,
+      ClientSite,
+      TeamMember,
+      ServiceRoute,
+      ServiceCheck,
+      SiteSignal,
+      WorkItem,
+    ]),
+  ],
+  controllers: [KobeSecurityController],
+  providers: [
+    SecurityClientsService,
+    ClientSitesService,
+    TeamMembersService,
+    ServiceRoutesService,
+    ServiceChecksService,
+    SiteSignalsService,
+    WorkItemsService,
+    KobeSecurityDashboardService,
+  ],
+})
+export class KobeSecurityModule {}
