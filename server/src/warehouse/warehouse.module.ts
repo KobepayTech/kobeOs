30f7f8e605f7f171068bf277ef818abe2c380984
KobepayTech <159016310+KobepayTech@users.noreply.github.com>
2026-05-30 11:45:37 +0300

    Register warehouse pick ticket repository

--- server/src/warehouse/warehouse.module.ts
+++ server/src/warehouse/warehouse.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { WarehouseItem, WarehouseMovement } from './warehouse.entity';
+import { WarehouseItem, WarehouseMovement, WarehousePickTicket } from './warehouse.entity';
 import { MovementsService, WarehouseItemsService } from './warehouse.service';
 import { WarehouseController } from './warehouse.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([WarehouseItem, WarehouseMovement])],
+  imports: [TypeOrmModule.forFeature([WarehouseItem, WarehouseMovement, WarehousePickTicket])],
   providers: [WarehouseItemsService, MovementsService],
   controllers: [WarehouseController],
+  exports: [TypeOrmModule],
 })
 export class WarehouseModule {}
