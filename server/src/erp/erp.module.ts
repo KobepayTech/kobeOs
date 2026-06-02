7286130b404204a35b2a9e6e9de77521b6f34668
KobepayTech <159016310+KobepayTech@users.noreply.github.com>
2026-06-02 11:15:44 +0300

    feat(erp): register supplier capital module pieces

--- server/src/erp/erp.module.ts
+++ server/src/erp/erp.module.ts
@@ -7,10 +7,33 @@ import { Contact } from '../contacts/contact.entity';
 import { PrintJob } from '../print/print.entity';
 import { ErpService } from './erp.service';
 import { ErpController } from './erp.controller';
+import {
+  ErpKobePayLink,
+  ErpKobePaySupplierReceipt,
+  ErpPurchaseOrder,
+  ErpSupplier,
+  ErpSupplierCapitalLedger,
+} from './supplier-capital.entity';
+import { SupplierCapitalService } from './supplier-capital.service';
+import { SupplierCapitalController } from './supplier-capital.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([PaymentTransaction, PosOrder, PosProduct, WarehouseItem, Contact, PrintJob])],
-  providers: [ErpService],
-  controllers: [ErpController],
+  imports: [
+    TypeOrmModule.forFeature([
+      PaymentTransaction,
+      PosOrder,
+      PosProduct,
+      WarehouseItem,
+      Contact,
+      PrintJob,
+      ErpKobePayLink,
+      ErpSupplier,
+      ErpPurchaseOrder,
+      ErpKobePaySupplierReceipt,
+      ErpSupplierCapitalLedger,
+    ]),
+  ],
+  providers: [ErpService, SupplierCapitalService],
+  controllers: [ErpController, SupplierCapitalController],
 })
 export class ErpModule {}
