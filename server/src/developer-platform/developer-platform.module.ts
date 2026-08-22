de674f617adfd31f748233459f67d17109cc4c08
KobepayTech <159016310+KobepayTech@users.noreply.github.com>
2026-08-22 03:22:36 -0700

    feat: register local Git developer service

--- server/src/developer-platform/developer-platform.module.ts
+++ server/src/developer-platform/developer-platform.module.ts
@@ -1,20 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AiModule } from '../ai/ai.module';
-import {
-  DeveloperAiController,
-  DeveloperPlatformController,
-} from './developer-platform.controller';
+import { DeveloperAiController, DeveloperPlatformController } from './developer-platform.controller';
+import { DeveloperGitController } from './developer-git.controller';
 import { DeveloperProject } from './developer-project.entity';
 import { DeveloperPlatformService } from './developer-platform.service';
+import { DeveloperGitService } from './developer-git.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([DeveloperProject]),
-    AiModule,
-  ],
-  controllers: [DeveloperPlatformController, DeveloperAiController],
-  providers: [DeveloperPlatformService],
-  exports: [DeveloperPlatformService],
+  imports: [TypeOrmModule.forFeature([DeveloperProject]), AiModule],
+  controllers: [DeveloperPlatformController, DeveloperAiController, DeveloperGitController],
+  providers: [DeveloperPlatformService, DeveloperGitService],
+  exports: [DeveloperPlatformService, DeveloperGitService],
 })
 export class DeveloperPlatformModule {}
