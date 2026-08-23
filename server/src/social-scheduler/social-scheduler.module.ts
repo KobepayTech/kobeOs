52a58e39d1f394f3242b550ba34e6c30cada6e31
KobepayTech <159016310+KobepayTech@users.noreply.github.com>
2026-08-23 00:37:47 -0700

    feat: register TikTok creator integration

--- server/src/social-scheduler/social-scheduler.module.ts
+++ server/src/social-scheduler/social-scheduler.module.ts
@@ -5,11 +5,13 @@ import { SocialPost } from './social-post.entity';
 import { SocialAccount } from './social-account.entity';
 import { SocialSchedulerService } from './social-scheduler.service';
 import { SocialSchedulerController } from './social-scheduler.controller';
+import { TikTokController, TikTokPublicController } from './tiktok.controller';
+import { TikTokService } from './tiktok.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([SocialPost, SocialAccount]), MediaModule],
-  providers: [SocialSchedulerService],
-  controllers: [SocialSchedulerController],
-  exports: [SocialSchedulerService],
+  providers: [SocialSchedulerService, TikTokService],
+  controllers: [SocialSchedulerController, TikTokController, TikTokPublicController],
+  exports: [SocialSchedulerService, TikTokService],
 })
 export class SocialSchedulerModule {}
